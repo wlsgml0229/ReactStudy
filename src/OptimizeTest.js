50737cdc2da924310b56cf9a7a58bc2cdb549153
wlsgml961010@gmail.com <wlsgml0229@naver.com>
2022-05-17 01:11:20 +0900

    react.memo 기능 학습

--- src/OptimizeTest.js
+++ src/OptimizeTest.js
@@ -1,34 +1,54 @@
 import React, { useState, useEffect } from "react";
 
-const TextView = React.memo(({ text }) => {
+// 변경이없음 -> 1에서 1 그대로
+const CounterA = React.memo(({ count }) => {
   useEffect(() => {
-    console.log(`Update :: Text : ${text}`);
-  });
-  return <div>{text}</div>;
-});
-
-const CountView = React.memo(({ count }) => {
-  useEffect(() => {
-    console.log(`Update :: Count : ${count}`);
+    console.log(`Update - CountA : ${count}`);
   });
   return <div>{count}</div>;
 });
 
+//obj 객체이기 때문에 얕은비교 --> 콘솔이 찍힌다.
+//얕은비교를 하지않도록 변경도 가능
+const CounterB = React.memo(({ obj }) => {
+  useEffect(() => {
+    console.log(`Update - CountB : ${obj.count}`);
+  });
+  return <div>{obj.count}</div>;
+});
+
+//true 면 리렌더 x false 리렌더 o
+const areEqual = (preProps, nextProps) => {
+  return preProps.obj.count === nextProps.obj.count;
+};
+
+const MemoizedCounterB = React.memo(CounterB, areEqual);
+
 const OptimizeTest = () => {
   const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
+  const [obj, setObj] = useState({
+    count: 1,
+  });
 
   return (
     <div style={{ padding: 50 }}>
       <div>
-        <h2>count</h2>
-        <CountView count={count}></CountView>
-        <button onClick={() => setCount(count + 1)}>+</button>
+        <h2>Count A</h2>
+        <CounterA count={count} />
+        <button onClick={() => setCount(count)}>A</button>
       </div>
       <div>
-        <h2>Text</h2>
-        <TextView text={text}></TextView>
-        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <h2>Count B</h2>
+        <MemoizedCounterB obj={obj} />
+        <button
+          onClick={() =>
+            setObj({
+              count: obj.count,
+            })
+          }
+        >
+          B
+        </button>
       </div>
     </div>
   );
